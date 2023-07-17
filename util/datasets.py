fe7c83000e993634ffa2e5b9178c727aa826c0ac
Jongseo Lee <jong980812@gamil.com>
2023-07-18 01:30:25 +0900

    pcb datasets

--- util/datasets.py
+++ util/datasets.py
@@ -20,6 +20,8 @@ from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 def build_dataset(is_train, args):
     if args.dataset == 'asd':
         transform = build_transform_asd(is_train, args)
+    elif args.dataset == 'pcb_asd':
+        transform = build_transform_pcb_asd(is_train, args)
     elif args.dataset == 'sketch_imagenet':    
         transform = build_transform_sketch_imagenet(is_train, args)
     elif args.dataset == 'tu_berlin':
@@ -37,6 +39,23 @@ def build_dataset(is_train, args):
 def build_transform_asd(is_train, args):
     data_transforms = {
             'train': transforms.Compose([
+                transforms.Resize((224,224)),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.96, 0.96, 0.96],
+                                        std=[0.1, 0.1, 0.1])
+                ]),
+                'val': transforms.Compose([
+                transforms.Resize((224,224)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.96, 0.96, 0.96],
+                                        std=[0.1, 0.1, 0.1])
+                ])}
+    
+    return data_transforms['train'] if is_train else data_transforms['val']#transforms.Compose(t)
+def build_transform_pcb_asd(is_train, args):
+    data_transforms = {
+            'train': transforms.Compose([
                 transforms.Resize((384,128)),
                 transforms.RandomHorizontalFlip(p=0.5),
                 transforms.ToTensor(),
@@ -49,26 +68,8 @@ def build_transform_asd(is_train, args):
                 transforms.Normalize(mean=[0.96, 0.96, 0.96],
                                         std=[0.1, 0.1, 0.1])
                 ])}
-    # data_transforms = {
-    #         'train': transforms.Compose([
-    #             transforms.RandomResizedCrop(112),
-    #             transforms.RandomHorizontalFlip(p=0.5),
-    #             transforms.ToTensor(),
-    #             transforms.Normalize(mean=[0.485, 0.456, 0.406],
-    #                                  std=[0.229, 0.224, 0.225])
-
-    #             ]),
-    #             'val': transforms.Compose([
-    #             transforms.Resize(256),
-    #             transforms.CenterCrop(224),
-    #             transforms.ToTensor(),
-    #             transforms.Normalize(mean=[0.485, 0.456, 0.406],
-    #                                  std=[0.229, 0.224, 0.225])
-
-    #             ])}
     
     return data_transforms['train'] if is_train else data_transforms['val']#transforms.Compose(t)
-
 def build_transform_sketch_imagenet(is_train, args):
     data_transforms = {
             'train': transforms.Compose([
