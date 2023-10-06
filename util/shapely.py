6e278a5d39af826e5f1bac4589805b2c60ed3390
Jongseo Lee <jong980812@gamil.com>
2023-10-07 04:10:40 +0900

    tempt

--- util/shapely.py
+++ util/shapely.py
@@ -9,7 +9,8 @@ import itertools
 from torchvision import datasets, transforms, models
 from custom_transform import ThresholdTransform,AddNoise,DetachWhite
 from einops import rearrange
-
+from itertools import product
+import math
 import torchvision.models as models
 model=models.efficientnet_b1(pretrained=True,progress=False)
 model.classifier[1] = torch.nn.Linear(1280, 2)
@@ -21,14 +22,96 @@ data_path='/data/datasets/asd/All_5split/01/val/TD/'
 # data_path='/data/datasets/ai_hub_sketch_4way/01/val/m_w'
 # data_path='/data/datasets/ai_hub/ai_hub_sketch_mw/01/val/w/'
 import random
-weight='/data/jong980812/project/mae/result_ver2/All_5split/binary_240/OUT/01/checkpoint-29.pth'
+weight='/data/jong980812/project/mae/result_ver2/All_5split/padding_mode_reflect/OUT/02/checkpoint-29.pth'
 checkpoint = torch.load(weight, map_location='cpu')
 print("Load pre-trained checkpoint from: %s" % weight)
 checkpoint_model = checkpoint['model']
 state_dict = model.state_dict()
 msg = model.load_state_dict(checkpoint_model, strict=False)
+def set_conv_padding_mode(model, padding_mode='replicate'):
+  for name, layer in model.named_modules():
+      if isinstance(layer, torch.nn.Conv2d):
+          layer.padding_mode = padding_mode
+set_conv_padding_mode(model,padding_mode='replicate')
 model.eval()
-print(msg)
+def get_shapely_matrix(all_ordered_pair, correct_output):
+    shapely_values = torch.zeros_like(all_ordered_pair, dtype=torch.float32)
+
+    # 각 ordered pair에 대한 값을 가져와 shapely_values에 저장
+    for a,ordered_pairs in enumerate(all_ordered_pair):
+        for i, ordered_pair in enumerate(ordered_pairs):
+            # ordered_pair를 인덱스로 사용하여 correct_output에서 값을 가져옴
+            indices = ordered_pair  # ordered_pair를 텐서로 변환
+            # print(indices)
+            values1 = correct_output[int(indices[0])]
+            values2 = correct_output[int(indices[1])]  # correct_output에서 해당 위치의 값 가져오기
+            # print(values1,values2)
+            shapely_values[a,i] = torch.cat([values1.unsqueeze(0),values2.unsqueeze(0)],dim=0)
+    return shapely_values
+def binary_to_decimal(binary_tuple):
+    decimal_value = 0
+    binary_length = len(binary_tuple)
+
+    for i, bit in enumerate(binary_tuple):
+        decimal_value += bit * (2 ** (binary_length - i - 1))
+
+    return decimal_value
+def decimal_to_binary(decimal_value, num_bits):
+    binary_tuple = []
+    
+    for i in range(num_bits):
+        bit = (decimal_value >> (num_bits - i - 1)) & 1
+        binary_tuple.append(bit)
+    
+    return tuple(binary_tuple)
+def count_ones(binary_tuple):
+    count = 0
+    for bit in binary_tuple:
+        if bit == 1:
+            count += 1
+    return count
+def get_ordered_pair():
+
+    n = 6  # digit의 개수
+    digits = [0, 1]  # 각 digit의 가능한 값
+
+    # 경우의 수 생성
+    part_combinations = list(product(digits, repeat=n))
+
+
+    index_to_insert = 1  # 두 번째 위치에 추가하려면 인덱스 1을 사용합니다.
+    all_ordered_pair=[]
+    for index in range(7):
+        ordered_pair=[] 
+        index_to_insert = index
+        for combi in part_combinations:
+            insert_value = [0,1]
+            new_combi_0= combi[:index_to_insert] + (insert_value[0],) + combi[index_to_insert:]
+            new_combi_1= combi[:index_to_insert] + (insert_value[1],) + combi[index_to_insert:]
+            ordered_pair.append([binary_to_decimal(new_combi_0),binary_to_decimal(new_combi_1)])
+        all_ordered_pair.append(ordered_pair)
+    all_ordered_pair=torch.Tensor(all_ordered_pair)
+    num_part = (all_ordered_pair.shape[0])
+    num_case = (all_ordered_pair.shape[1])
+    weights = torch.zeros((num_part,num_case))
+    for i in range(num_part):
+        for j in range(num_case):
+            # all_ordered_pair의 값 가져오기
+            value = int(all_ordered_pair[i, j, 1])
+            
+            # 이진수로 변환
+            binary_value = decimal_to_binary(value, 7)
+            
+            # 1의 개수 세기
+            num_ones = binary_value.count(1)
+            
+            # num * (7 combination num) 계산
+            combination = math.comb(num_part,num_ones)
+            weight = num_ones * combination
+            
+            # 결과를 weights에 저장
+            weights[i, j] = weight
+    return all_ordered_pair, weights
 class shapely_part(Dataset):
     def __init__(self, data_folder, json_folder, binary_thresholding=None, transform=None):
         self.json_folder = json_folder
@@ -224,9 +307,13 @@ class shapely_part(Dataset):
                 new_img=self.transform(new_img)
             new_imgs.append(new_img.unsqueeze(0))
         new_imgs = torch.cat(new_imgs,dim=0)
-        return new_imgs,label 
+        image = self.transform(image)
+        image_3ch = image.expand(3,-1,-1)
+        return new_imgs,image_3ch,label 
     
     
+
+
 
 
 if __name__=="__main__":
@@ -234,7 +321,10 @@ if __name__=="__main__":
     dataset = shapely_part('/data/jong980812/project/mae/util','/data/jong980812/project/mae/util',240,transform=transform)
     data_loader=DataLoader(dataset,2,num_workers=4)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    for new_imgs,label in data_loader:
+    
+    
+    all_ordered_pair,weights = get_ordered_pair()
+    for new_imgs,original_image,label in data_loader:
         print(new_imgs.shape)
         input_data = new_imgs
         print('complete')
@@ -243,10 +333,19 @@ if __name__=="__main__":
         
         model.to(device)
         input_data = input_data.to(device)
+        original_image = original_image.to(device)
         label = label.to(device)
         model.eval()
         with torch.no_grad():
+            prediction = model(original_image)
             output=model(input_data)
         output = rearrange(output, '(b t) o -> b t o', b=batch_size) # batch_size, 128, output(2)
         print(output.shape)
         print(label)
+        for i in range(batch_size):
+            correct_output = output[:,:,label[i]]# Take correct logits,  (b, 128), 밖에서. 
+            shapely_matrix = get_shapely_matrix(all_ordered_pair,correct_output[i])
+            shapely_contributions = shapely_matrix[:,:,1] - shapely_matrix[:,:,0] 
+            shapely_value = (shapely_contributions * 1/weights).sum(dim=1)
+            print(shapely_value)
+        print(prediction)
